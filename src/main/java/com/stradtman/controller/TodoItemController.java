2f4ee8cb82fbe256c4c265abd32edcabb8a30cd1
Kevin Stradtman <stradtkt@gmail.com>
2018-11-26 13:21:10 -0500

    Added viewItem

--- src/main/java/com/stradtman/controller/TodoItemController.java
+++ src/main/java/com/stradtman/controller/TodoItemController.java
@@ -40,15 +40,23 @@ public class TodoItemController {
     }
 
     @GetMapping(Mappings.ADD_ITEM)
-    public String addEditItem(Model model) {
-        TodoItem todoItem = new TodoItem("", "", LocalDate.now());
+    public String addEditItem(@RequestParam(required = false, defaultValue = "-1") int id, Model model) {
+        log.info("editing id = {}", id);
+        TodoItem todoItem = todoItemService.getItem(id);
+        if(todoItem == null) {
+            todoItem = new TodoItem("", "", LocalDate.now());
+        }
         model.addAttribute(AttributeNames.TODO_ITEM, todoItem);
         return ViewNames.ADD_ITEM;
     }
     @PostMapping(Mappings.ADD_ITEM)
     public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) TodoItem todoItem) {
         log.info("TodoItem from = {}", todoItem);
-        todoItemService.addItem(todoItem);
+        if(todoItem.getId() == 0) {
+            todoItemService.addItem(todoItem);
+        } else {
+            todoItemService.updateItem(todoItem);
+        }
         return "redirect:/" + Mappings.ITEMS;
     }
     @GetMapping(Mappings.DELETE_ITEM)
@@ -56,5 +64,11 @@ public class TodoItemController {
         log.info("Deleting item with id = {}", id);
         todoItemService.removeItem(id);
         return "redirect:/" + Mappings.ITEMS;
+    }
+    @GetMapping(Mappings.VIEW_ITEM)
+    public String viewItem(@RequestParam int id, Model model) {
+        TodoItem todoItem = todoItemService.getItem(id);
+        model.addAttribute(AttributeNames.TODO_ITEM, todoItem);
+        return ViewNames.VIEW_ITEM;
     }
 }
---
+++ src/main/webapp/WEB-INF/view/view_item.jsp
@@ -0,0 +1,32 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ page import="com.stradtman.utils.Mappings" %>
+<html>
+<head>
+    <title>Add Items</title>
+</head>
+<body>
+    <div align="center">
+        <table>
+            <tr>
+                <td><label>ID</label></td>
+                <td><c:out value="${todoItem.id}" /></td>
+            </tr>
+            <tr>
+                <td><label>Title</label></td>
+                <td><c:out value="${todoItem.title}" /></td>
+            </tr>
+            <tr>
+                <td><label>DeadLine</label></td>
+                <td><c:out value="${todoItem.deadline}" /></td>
+            </tr>
+            <tr>
+                <td><label>Details</label></td>
+                <td><c:out value="${todoItem.details}" /></td>
+            </tr>
+        </table>
+
+        <c:url var="tableUrl" value="${Mappings.ITEMS}"/>
+        <a href="${tableUrl}">Show Table</a>
+    </div>
+</body>
+</html>
